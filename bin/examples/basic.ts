ee1d73d94a4d92b411882e84bf0f3936e9d7f138
Rick Gigger <rickg@gradecam.com>
2017-11-09 13:32:43 -0700

    example script for generating a basic pdf file

---
+++ bin/examples/basic.ts
@@ -0,0 +1,8 @@
+import * as fpdf from '../../src/fpdf';
+
+const pdf = new fpdf.FPdf();
+
+pdf.addPage();
+pdf.setFont('Helvetica', '', 16);
+pdf.text(100, 100, 'Testing 1 2 3');
+pdf.output('examples/embedded.pdf');
